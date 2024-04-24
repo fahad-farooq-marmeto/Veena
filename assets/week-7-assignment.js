03dfaa0ea31e947dafb75025bbc29e8e6a528479
Fahad <farooqrukhbaiz@gmail.com>
2024-04-24 19:17:20 +0530

    finished assignment

---
+++ assets/week-7-assignment.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('.hotspot').forEach(hotspot => {
+    hotspot.addEventListener('mouseover', function() {
+      this.querySelector('.hotspot__product-card').style.display = 'block';
+    });
+  
+    hotspot.addEventListener('mouseout', function() {
+      this.querySelector('.hotspot__product-card').style.display = 'none';
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#product-slider', {
+      type: 'loop',
+      pagination: false,
+      perPage: 1,
+      perMove: 1,
+      breakpoints: {
+        640: {
+          perPage: 1,
+        },
+      },
+    }).mount();
+  });
